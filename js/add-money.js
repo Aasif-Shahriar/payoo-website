b021d46efff243ac7d5dd76835844b9a657a6860
Aasif-Shahriar <asifshahriar247@gmail.com>
2025-02-25 19:46:06 +0600

    added add money logic

---
+++ js/add-money.js
@@ -0,0 +1,19 @@
+const addMoney = document.getElementById('add-money')
+
+addMoney.addEventListener('click', (event) => {
+    event.preventDefault();
+    const amountEl = document.getElementById('amount').value;
+    const convertedAmount = parseFloat(amountEl);
+    const pin = document.getElementById('pin').value;
+    const convertedPin = parseInt(pin);
+    const balance = document.getElementById('balance').innerText;
+    const convertedBalance = parseFloat(balance);
+
+    if (convertedPin === 1234) {
+        let sum = convertedAmount + convertedBalance;
+        document.getElementById('balance').innerText = sum;
+    }
+    else{
+        alert('you can not enter')
+    }
+})
